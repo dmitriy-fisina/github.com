ae444e87aae7beebf44f5e71f39de6076fc6d615
dmitriy-fisina <44885839+dmitriy-fisina@users.noreply.github.com>
2018-11-09 04:37:14 +0200

    Cognifide-test

---
+++ Cognifide-tes/Js/script.js
@@ -0,0 +1,29 @@
+
+var map;
+function initMap() {
+    
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        
+        center: {lat: 43.729311, lng: -79.432360},
+        zoom: 15,
+        disableDefaultUI: true
+    });
+    var icon = {
+        url: "../Image/marker2.png", // url
+        scaledSize: new google.maps.Size(28, 40), // scaled size
+        origin: new google.maps.Point(0,0), // origin
+        anchor: new google.maps.Point(0, 0) // anchor
+    };
+
+    var marker = new google.maps.Marker({
+
+        position: {lat: 43.729243, lng: -79.443798},
+
+        map: map,
+
+        title: 'Marker',
+
+        icon: icon
+    });
+}

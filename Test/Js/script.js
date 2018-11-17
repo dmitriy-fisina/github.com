105ed12c6e1d258fc6cb0c40a7ef440683b5d87d
dmitriy-fisina <44885839+dmitriy-fisina@users.noreply.github.com>
2018-11-17 19:56:15 +0200

    test

---
+++ Test/Js/script.js
@@ -0,0 +1,27 @@
+function initMap() {
+    
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        
+        center: {lat: 43.729311, lng: -79.432360},
+        zoom: 15,
+        disableDefaultUI: true
+    });
+    var icon = {
+        url: "Image/mark.png", // url
+        scaledSize: new google.maps.Size(28, 40), // scaled size
+        origin: new google.maps.Point(0,0), // origin
+        anchor: new google.maps.Point(0, 0) // anchor
+    };
+
+    var marker = new google.maps.Marker({
+
+        position: {lat: 43.729243, lng: -79.443798},
+
+        map: map,
+
+        title: 'Marker',
+
+        icon: icon
+    });
+}

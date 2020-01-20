e05b39526791e91fa5be4b9bb1d54f1eacdbd6b2
dmitriyfisina <44885839+dmitriyfisina@users.noreply.github.com>
2020-01-20 15:54:48 +0200

    delete old files

--- Logicify_tz/Js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $(".burger-menu-icon").click(function () {
-        $(".menu-mobile").addClass("menu-show");
-    });
-    $(".icon-close").click(function () {
-        $(".menu-mobile").removeClass("menu-show");
-    });
-});

fa90be1915ac5bd8a3cf743f7a2dedcfb6e2d5b3
dmitriyfisina <44885839+dmitriyfisina@users.noreply.github.com>
2020-04-30 20:51:51 +0300

    update

--- dist/js/main.js
+++ dist/js/main.js
@@ -28,4 +28,11 @@ $(document).ready(function () {
             }
           ]
     });
+    $('[href *= "#"]').on("click", function (e) {
+      var anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $(anchor.attr("href")).offset().top
+      }, 1000);
+      e.preventDefault();
+  });
 });
